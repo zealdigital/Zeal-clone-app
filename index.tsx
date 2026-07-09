3d48895b2693f4dc122ac006d1604ace81957e36
zealdigital <74176798+zealdigital@users.noreply.github.com>
2026-07-09 11:30:41 +1000

    Import CSS file in index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'services/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
