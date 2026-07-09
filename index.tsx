5b5e61c99169dfe761a5af41455e3e50c03db786
zealdigital <74176798+zealdigital@users.noreply.github.com>
2026-07-09 11:52:21 +1000

    Remove unused CSS import
    Remove unused CSS import from index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'services/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
