53229827bc6a7be6ad6e0868d1a45b3eff102115
zealdigital <74176798+zealdigital@users.noreply.github.com>
2026-08-12 15:46:14 +1000

    Replace 'Assigned BDM' header with 'Notes'

--- components/ManagerDashboard.tsx
+++ components/ManagerDashboard.tsx
@@ -1178,7 +1178,7 @@ const parseTimeStringToMinutes = (timeStr: string): number => {
                                                             <th className="px-6 py-4 text-left text-xs font-bold text-white uppercase tracking-widest">Time</th>
                                                             <th className="px-6 py-4 text-left text-xs font-bold text-white uppercase tracking-widest">Status</th>
                                                             <th className="px-6 py-4 text-left text-xs font-bold text-white uppercase tracking-widest">Region</th>
-                                                            <th className="px-6 py-4 text-left text-xs font-bold text-white uppercase tracking-widest">Assigned BDM	</th>
+                                                            <th className="px-6 py-4 text-left text-xs font-bold text-white uppercase tracking-widest">Notes</th>
                                                             <th className="px-6 py-4 text-right text-xs font-bold text-white uppercase tracking-widest">Actions</th>
                                                         </tr>
                                                     </thead>
